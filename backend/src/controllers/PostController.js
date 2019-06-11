a7eefd5a02217a4eca884fc63d136ef19e3bda79
Mateus Miguel <webmateusmiguel@gmail.com>
2019-06-11 17:28:44 -0300

    Aula 1

---
+++ backend/src/controllers/PostController.js
@@ -0,0 +1,10 @@
+const post = require("../models/Post");
+
+module.exports = {
+  async index(req, res) {},
+
+  async store(req, res) {
+    console.log(req.file);
+    return res.json({ ok: true });
+  }
+};
